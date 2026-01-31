f42041c68cd1284d75e1e528bf1a505f24060e77
RomainNykyforchyn <romainnykyforchyn@proton.me>
2026-01-31 20:18:10 +0100

    feat: add pickup item logic with example

--- home-is-toxic/scripts/PickupItem.gd
+++ home-is-toxic/scripts/PickupItem.gd
@@ -1,8 +1,31 @@
 extends Interactable
 class_name PickupItem
 
-@export var item_id: StringName = &"key"
+@export var item_id: StringName = &"test_item"
 @export var amount: int = 1
+@export var icon: Texture2D
+
+# Choose one:
+@export var collision_radius: float = 10.0
+# @export var collision_size: Vector2 = Vector2(16, 16) # for rectangle
+
+@onready var sprite: Sprite2D = $Sprite2D
+@onready var colshape: CollisionShape2D = $CollisionShape2D
+
+func _ready() -> void:
+	if icon:
+		sprite.texture = icon
+
+	# Circle collision
+	var shape := CircleShape2D.new()
+	shape.radius = collision_radius
+	colshape.shape = shape
+
+	# If you prefer rectangle collision, use this instead:
+	# var shape := RectangleShape2D.new()
+	# shape.size = collision_size
+	# colshape.shape = shape
+
 
 func interact(_player: Node) -> void:
 	# Add to inventory (see section below)
--- home-is-toxic/scripts/Player.gd
+++ home-is-toxic/scripts/Player.gd
@@ -12,6 +12,8 @@ signal health_changed(new_health: int)
 
 func _ready() -> void:
 	health_changed.emit(health)
+	interact_area.area_entered.connect(_on_interact_area_area_entered)
+	interact_area.area_exited.connect(_on_interact_area_area_exited)
 
 func _physics_process(_delta):
 	var dir = Input.get_vector("left", "right", "up", "down")
@@ -38,7 +40,6 @@ func _pick_best() -> void:
 			best_dist = d
 			best = it
 	current = best
-
 func apply_damage(damage: int) -> void:
 	health -= damage
 	health_changed.emit(health)
@@ -48,12 +49,14 @@ func set_health(new_health: int) -> void:
 	health_changed.emit(health)
 
 func _on_interact_area_area_entered(area: Area2D) -> void:
+	print("entered area", area)
 	if area is Interactable:
 		nearby.append(area)
 		_pick_best()
 
 
 func _on_interact_area_area_exited(area: Area2D) -> void:
+	print("exited area", area)
 	if area is Interactable:
 		nearby.erase(area)
 		_pick_best()
